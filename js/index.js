3cae7940e04f13b580451983a80167521ae76749
Marianita <mb.martinezgutierrez@ugto.mx>
2023-08-21 11:07:04 -0600

    Terminamos la práctica

---
+++ js/index.js
@@ -0,0 +1,27 @@
+console.log('conectado')
+
+const titulo = document.getElementsByClassName('titulo')[0]
+titulo.style.color = 'red'
+
+titulo.addEventListener('hover', () => {
+    titulo.style.color = 'black'
+})
+console.log('titulo', titulo)
+
+const num1 = document.getElementById('num1')
+const num2 = document.getElementById('num2')
+const btnSuma = document.getElementById('btnSuma')
+
+btnSuma.addEventListener('click', () => {
+    sumaValores()
+})
+
+const sumaValores = () => {
+    console.log('valores', parseInt(num1.value), parseInt(num2.value))
+    if(!isNaN(parseInt(num1.value))){
+        if(!isNaN(parseInt(num2.value))){
+            const res = parseInt(num1.value) + parseInt(num1.value)
+            alert('El resultado es: ' + res)
+        }
+    }
+}
